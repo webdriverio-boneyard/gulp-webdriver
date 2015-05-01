0c2cc9da81883bc2f5e1bcf3dd8d63a34ac047b0
christian-bromann <contact@christian-bromann.com>
2015-05-01 12:34:33 +0200

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var webdriver = require('./index');
+
+gulp.task('test:chrome_ci', function() {
+    return gulp.src('test/*.js', {
+        read: false
+    }).pipe(webdriver({
+        updateSauceJob: true,
+        logLevel: 'verbose',
+        user: process.env.SAUCE_USERNAME,
+        key: process.env.SAUCE_ACCESS_KEY,
+        host: 'ondemand.saucelabs.com',
+        port: 80,
+        desiredCapabilities: {
+            browserName: 'chrome',
+            platform: 'Windows 8',
+            version: '31',
+            tags: ['chrome', 'Windows 8', '31'],
+            name: 'grunt-webdriver test',
+            build: process.env.TRAVIS_BUILD_NUMBER
+        }
+    }));
+});
+
+gulp.task('test:chrome_ciTunnel', function() {
+    return gulp.src('test/*.js', {
+        read: false
+    }).pipe(webdriver({
+        updateSauceJob: true,
+        logLevel: 'verbose',
+        user: process.env.SAUCE_USERNAME,
+        key: process.env.SAUCE_ACCESS_KEY,
+        port: 4445,
+        desiredCapabilities: {
+            browserName: 'chrome',
+            platform: 'Windows 8',
+            version: '31',
+            tags: ['chrome', 'Windows 8', '31'],
+            name: 'grunt-webdriver test',
+            'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+            build: process.env.TRAVIS_BUILD_NUMBER
+        }
+    }));
+});
+
+gulp.task('test:local', function() {
+    return gulp.src('test/*.js', {
+        read: false
+    }).pipe(webdriver({
+        logLevel: 'verbose',
+        desiredCapabilities: {
+            browserName: 'phantomjs'
+        }
+    }));
+});
