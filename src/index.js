2f90b72371640bb144d048913caea6ae88f6bd25
Alex Zaslavsky <alexz@speedshare.com>
2016-07-05 23:27:32 -0700

    simplified some code

--- src/index.js
+++ src/index.js
@@ -12,15 +12,14 @@ module.exports = (options) => {
         wdio.run().then(code => {
             process.stdin.pause()
 
-            if (code === 0) {
-                process.nextTick(() => stream.emit('end'))
-            } else {
+            if (code !== 0) {
                 process.nextTick(() => stream.emit('error', new gutil.PluginError('gulp-webdriver', `wdio exited with code ${code}`, {
                     showStack: false
                 })))
             }
 
             callback()
+            process.nextTick(() => stream.emit('end'))
         }, e => {
             process.stdin.pause()
             process.nextTick(() => stream.emit('error', new gutil.PluginError('gulp-webdriver', e, { showStack: true })))
